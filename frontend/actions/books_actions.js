b365734b38d5cd56c063a0eb60ff37ad4ed215c9
Thomas Vu <mr.thomas.vu@gmail.com>
2017-11-22 21:10:05 -0800

    Added book ajax and actions

---
+++ frontend/actions/books_actions.js
@@ -0,0 +1,24 @@
+import * as APIUtil from '../util/books_api_util';
+
+export const RECEIVE_BOOKS = 'RECEIVE_BOOKS';
+export const RECEIVE_BOOK = 'RECEIVE_BOOK';
+
+const receiveBooks = (books) => ({
+	type: RECEIVE_BOOKS,
+	books
+});
+
+const receiveBook = (book) => ({
+	type: RECEIVE_BOOK,
+	book
+});
+
+export const getAllBooks = () => dispatch => (
+	APIUtil.getAllBooks().then(
+		(books) => dispatch(receiveBooks(books)) )
+);
+
+export const getSingleBook = id => dispatch => (
+	APIUtil.getSingleBook(id).then(
+		(book) => dispatch(receiveBook(book)) )
+);
---
+++ frontend/util/bookshelves_api_util.js
@@ -0,0 +1,30 @@
+export const getBookshelf = (id) => (
+	$.ajax({
+		method: 'GET',
+		url: `/api/bookshelves/${id}`
+	})
+);
+
+export const createBookshelf = (bookshelf) => (
+	$.ajax({
+		method: 'POST',
+		url: '/api/bookshelves',
+		data: { bookshelf }
+	})
+);
+
+export const deleteBookshelf = (id) => (
+	$.ajax({
+		method: 'DELETE',
+		url: `/api/bookshelves/${id}`
+	})
+);
+
+export const editBookshelf = (bookshelf) => (
+	$.ajax({
+		method: 'PATCH',
+		url: `/api/bookshelves/${bookshelf.id}`
+		data: { bookshelf }
+	})
+);
+
