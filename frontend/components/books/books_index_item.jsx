3e5371259065fb34c45726bc67984f07b16d9316
Thomas Vu <mr.thomas.vu@gmail.com>
2017-11-24 10:50:08 -0800

    Minor styling done on books index

--- frontend/components/books/books_index_item.jsx
+++ frontend/components/books/books_index_item.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default ({ book }) => (
-	<li className= 'book-title'>
-		{ book.title }
+	<li className= 'book-info'>
+		<div className= 'book-splash'>
+			<div className= 'book-title'>
+				<label>Title:   
+					{ book.title }
+				</label>
+
+				<label>Author:   
+					{ book.author }
+				</label>
+			</div>
+		</div>
+
+		<div className= 'book-review'>
+			<div className= 'book-summary'>
+				{ book.summary }
+			</div>
+			<div className= 'book-stars'>
+				<i className="fa fa-star-o fa-lg" aria-hidden="true"></i>
+			</div>
+		</div>
 	</li>
 );
