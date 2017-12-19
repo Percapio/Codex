cd648c81e32f94565924285ccb91717cb976b45e
Thomas Vu <mr.thomas.vu@gmail.com>
2017-12-19 14:47:06 -0800

    fixed heroku errors

--- app/controllers/api/thumbs_controller.rb
+++ app/controllers/api/thumbs_controller.rb
@@ -1,11 +1,19 @@
 class Api::ThumbsController < ApplicationController
   def create
-  	@thumb = Thumb.new(user_id: current_user.id, book_id: params[:book_id], up: params[:up])
+    @thumb = Thumb.find_by(user_id: current_user.id, book_id: params[:book_id])
 
-  	if @thumb.save
-  		@book = @thumb.book
-  		render 'api/books/show'
-  	else
+    if @thumb.nil?
+      @thumb = Thumb.new(user_id: current_user.id, book_id: params[:book_id], up: params[:up])
+    else
+      @thumb.up = params[:up]
+    end
+
+    if @thumb.save
+      @book = @thumb.book
+      render 'api/books/show'
+    else
+        debugger
+
   		render json: @thumb.errors.full_messages, status: 401
   	end
   end
--- frontend/components/extra_components/errors/errors_container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { selectAllErrors } from '../../selectors/selectors';
-import App from '../../app';
-
-const mapStateToProps = state => ({
-	errors: selectAllErrors(state)
-});
-
-export default connect(mapStateToProps, null)(App);
